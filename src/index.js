ad882a21e56072d8161988ee9812a41fabb3306f
Christian Coleman <christian.c2508@gmail.com>
2020-07-26 15:38:04 -0400

    added dependencies | partial redux setup | no router setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import loggerMiddleware from './middleware/logger'
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [loggerMiddleware, ...getDefaultMiddleware()]
+})
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
