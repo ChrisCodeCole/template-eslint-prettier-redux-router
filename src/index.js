7e0dc51e449eced63ce1929d3a7b9fd45e265476
Christian Coleman <christian.c2508@gmail.com>
2020-07-26 18:51:08 -0400

    updated redux config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import loggerMiddleware from './middleware/logger'
+import store from './store'
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: [loggerMiddleware, ...getDefaultMiddleware()]
-})
-
-if (process.env.NODE_ENV !== 'production' && module.hot) {
-  module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
-}
+// if (process.env.NODE_ENV !== 'production' && module.hot) {
+//   module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+// }
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import loggerMiddleware from './middleware/logger'
+import counterReducer from './reducers/counterSlice'
+
+const store = configureStore({
+  reducer: {
+      counter: counterReducer
+  },
+  middleware: [loggerMiddleware, ...getDefaultMiddleware()]
+})
+
+export default store;
